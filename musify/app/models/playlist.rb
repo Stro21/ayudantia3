0fc2efd65a8bc2bd5e81441abd5b1f3c1f828151
Eduardo Straub <eduardo.straub1@gmail.com>
2019-03-21 20:37:38 -0300

    Se ponen las relaciones

--- musify/app/models/playlist.rb
+++ musify/app/models/playlist.rb
@@ -1,3 +1,10 @@
 class Playlist < ApplicationRecord
   belongs_to :user
+
+  #Sin modelo intermedio
+  #has_and_belongs_to_many :songs
+
+  #Con modelo intermedio
+  has_many :playlist_songs
+  has_many :songs, throught: :playlist_songs
 end
--- musify/app/models/song.rb
+++ musify/app/models/song.rb
@@ -1,2 +1,8 @@
 class Song < ApplicationRecord
+  #Sin modelo intermedio
+  #has_and_belongs_to_many :playlists
+
+  #Con modelo intermedio
+  has_many :playlist_songs
+  has_many :playlists, through: :playlist_songs
 end
